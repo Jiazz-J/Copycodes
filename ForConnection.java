3b796385df417f9181dc5a25e82fee324c8f0c84
Jiazz-J <39787706+Jiazz-J@users.noreply.github.com>
2019-08-14 18:32:18 +0530

    Create ForConnection.java

---
+++ ForConnection.java
@@ -0,0 +1,17 @@
+package com.serverside;
+
+import java.sql.*;
+
+public class ForConnection {
+	
+	
+	
+
+
+static Connection con() throws ClassNotFoundException,SQLException {
+	Class.forName("oracle.jdbc.driver.OracleDriver");
+	Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","dbUser","hello");
+	return con;
+}
+
+}
