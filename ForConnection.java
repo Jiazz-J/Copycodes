2f5457a124398909036c8a9cd7218e0809249299
Jiazz-J <39787706+Jiazz-J@users.noreply.github.com>
2021-02-17 00:56:55 +0530

    Update ForConnection.java

--- ForConnection.java
+++ ForConnection.java
@@ -3,15 +3,11 @@ package com.serverside;
 import java.sql.*;
 
 public class ForConnection {
-	
-	
-	
 
-
-static Connection con() throws ClassNotFoundException,SQLException {
-	Class.forName("oracle.jdbc.driver.OracleDriver");
-	Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","dbUser","hello");
-	return con;
-}
+	static Connection con() throws ClassNotFoundException,SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","dbUser","hello");
+		return con;
+	}
 
 }
